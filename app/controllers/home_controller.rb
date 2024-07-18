b552b95a395e3e1f965db54bab09021e7982da5f
Er Whey <erwhey@gmail.com>
2024-07-18 13:58:47 +0800

    improve look

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @fluid_intakes = FluidIntake.all
-    @food_intakes = FoodIntake.all
+    @fluid_intakes = FluidIntake.all.order(created_at: :desc).limit(10)
+    @food_intakes = FoodIntake.all.order(created_at: :desc).limit(10)
   end
 end
